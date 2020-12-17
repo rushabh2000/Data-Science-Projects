ec064fa62f00ea035267530cfeedb434ffc5c2c3
rushabh2000 <54415984+rushabh2000@users.noreply.github.com>
2020-12-17 12:52:06 +0530

    Update regularize-cv.py

--- project1/regularize-cv.py
+++ project1/regularize-cv.py
@@ -26,7 +26,7 @@ def main():
     [X_train, trn_mean, trn_std] = normalize_train(X_train)
     X_test = normalize_test(X_test, trn_mean, trn_std)
 
-    #Define the range of lambda to test
+    
     lmbda = np.logspace(-1.00,2.00,101)
 
     #lmbda = [1,100]
@@ -46,7 +46,7 @@ def main():
 
 
     #Plot the MSE as a function of lmbda
-    #fill in
+   
     #plt.plot(lmbda,MSE,label = 'lamda =' + str(lmbda))
     #plt.title('relationship between mean squared error and lambda values')
     #plt.ylabel(" MSE values")
@@ -65,14 +65,9 @@ def main():
     print(y_result)
     return model_best
 
-
-#Function that normalizes features in training set to zero mean and unit variance.
-#Input: training data X_train
-#Output: the normalized version of the feature matrix: X, the mean of each column in
-#training set: trn_mean, the std dev of each column in training set: trn_std.
 def normalize_train(X_train):
 
-    #fill in
+   
     mean = np.mean(X_train,axis = 0)
     std = np.std(X_train, axis=0)
     X = (X_train - mean) / std
@@ -82,44 +77,31 @@ def normalize_train(X_train):
     return X, mean, std
 
 
-#Function that normalizes testing set according to mean and std of training set
-#Input: testing data: X_test, mean of each column in training set: trn_mean, standard deviation of each
-#column in training set: trn_std
-#Output: X, the normalized version of the feature matrix, X_test.
 def normalize_test(X_test, trn_mean, trn_std):
 
-    #fill in
+  =
     X = (X_test - trn_mean) / trn_std
 
 
     return X
 
-
-
-#Function that trains a ridge regression model on the input dataset with lambda=l.
-#Input: Feature matrix X, target variable vector y, regularization parameter l.
-#Output: model, a numpy object containing the trained model.
 def train_model(X,y,l):
 
-    #fill in
     model = linear_model.Ridge(alpha =l,fit_intercept = True)
     model.fit(X,y)
     return model
 
 
-#Function that calculates the mean squared error of the model on the input dataset.
-#Input: Feature matrix X, target variable vector y, numpy model object
-#Output: mse, the mean squared error
 def error(X,y,model):
 
-    #Fill in
+    
     yn = model.predict(X)
     mse = mean_squared_error(y,yn)
     return mse
 
 if __name__ == '__main__':
     model_best = main()
-    #We use the following functions to obtain the model parameters instead of model_best.get_params()
+   
     diamonds = pd.read_csv('diamonds.csv')
 
     # Feature and target matrices
